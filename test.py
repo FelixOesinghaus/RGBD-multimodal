b0915480a8afa04b298030ca6b9198f66eb9b9ac
FelixOesinghaus <58511024+FelixOesinghaus@users.noreply.github.com>
2020-12-08 14:22:06 +0100

    tests1

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import pathlib
 import os
 import PIL
 import PIL.Image
@@ -8,3 +10,48 @@ import tensorflow_datasets as tfds
 print(tf.__version__)
 
 print("test")
+
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file(origin=dataset_url, 
+                                   fname='flower_photos', 
+                                   untar=True)
+data_dir = pathlib.Path(data_dir)
+
+image_count = len(list(data_dir.glob('*/*.jpg')))
+print(image_count)
+
+roses = list(data_dir.glob('roses/*'))
+PIL.Image.open(str(roses[0]))
+
+batch_size = 32
+img_height = 180
+img_width = 180
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+  
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+  
+class_names = train_ds.class_names
+print(class_names)
+
+
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+  for i in range(9):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.title(class_names[labels[i]])
+    plt.axis("off")
