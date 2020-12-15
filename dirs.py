1cfb268282e608fc8da8d3e601dfa74f3ad1c16b
Lucas <lucas.olbertz@gmail.com>
2020-12-15 14:23:07 +0100

    works a bit?

--- dirs.py
+++ dirs.py
@@ -7,6 +7,9 @@ import shutil
 import PIL
 import PIL.Image
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
 import tensorflow_datasets as tfds
 
 print(tf.__version__)
@@ -104,10 +107,77 @@ val_ds = tf.keras.preprocessing.image_dataset_from_directory(
 class_names = train_ds.class_names
 
 # plt.figure(figsize=(10, 10))
-for images, labels in train_ds.take(1):
-	for i in range(9):
-		# ax = plt.subplot(3, 3, i + 1)
-		plt.imshow(images[i].numpy().astype("uint8"))
-		plt.title(class_names[labels[i]])
-		plt.axis("off")
-		plt.show()
+# for images, labels in train_ds.take(1):
+# 	for i in range(9):
+# 		# ax = plt.subplot(3, 3, i + 1)
+# 		plt.imshow(images[i].numpy().astype("uint8"))
+# 		plt.title(class_names[labels[i]])
+# 		plt.axis("off")
+# 		plt.show()
+
+normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
+
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+# Notice the pixels values are now in `[0,1]`.
+# print(np.min(first_image), np.max(first_image))
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+num_classes = len(class_names)
+# num_classes = 5
+
+model = tf.keras.Sequential([
+	layers.experimental.preprocessing.Rescaling(1./255),
+	layers.Conv2D(32, 3, activation='relu'),
+	layers.MaxPooling2D(),
+	layers.Conv2D(32, 3, activation='relu'),
+	layers.MaxPooling2D(),
+	layers.Conv2D(32, 3, activation='relu'),
+	layers.MaxPooling2D(),
+	layers.Flatten(),
+	layers.Dense(128, activation='relu'),
+	layers.Dense(num_classes)
+])
+
+model.compile(
+	optimizer='adam',
+	loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+	metrics=['accuracy'])
+
+model.fit(
+	train_ds,
+	validation_data=val_ds,
+	epochs=3
+)
+
+model.save(os.getcwd())
+
+history = model.history
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(3)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
