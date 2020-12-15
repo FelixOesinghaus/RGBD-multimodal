c4ff2197732a89c5fb76d922d6dc559779171a78
Lucas <lucas.olbertz@gmail.com>
2020-12-15 13:49:13 +0100

    another try

--- my_test.py
+++ my_test.py
@@ -22,34 +22,33 @@ print("test")
 
 #print(data_dir)
 
-paths, dirs, files = next(os.walk(os.getcwd()))
+os.mkdir(os.getcwd() + "\\crop")
+
+paths, dirs, files = next(os.walk(os.getcwd() + "\\rgbd-dataset\\"))
 print(dirs)
 for directory in dirs:
     if "apple" in directory:
         applepath = os.getcwd() + "\\" + directory
-        print(applepath)
-        # tmpapplepath = applepath + "\\" + "*.png"
-        # print(Path(tmpapplepath))
-        # print(Path.cwd())
-data_dir = pathlib.Path(applepath)
+        data_dir = pathlib.Path(applepath)
 
-path, dirs, files = next(os.walk(applepath))
-for directory in dirs:
-    if "apple" in directory:
-        subapplepath = applepath + "\\" + directory
-        print("sub: " + subapplepath)
-path, dirs, files = next(os.walk(subapplepath))
-print("-------------------")
-print(subapplepath)
-print("-------------------")
-applepics = []
-for file in files:
-    if "crop.png" in file:
-        picpath = subapplepath + "\\" + file
-        applepics.append(picpath)
+        subpath, subdirs, subfiles = next(os.walk(applepath))
 
-print(len(applepics))
-print(applepics[0])
+        for subdirectory in subdirs:
+            if "apple" in subdirectory:
+                subapplepath = applepath + "\\" + subdirectory
+                print("sub: " + subapplepath)
+                path, dirs, files = next(os.walk(subapplepath))
+# print("-------------------")
+# print(subapplepath)
+# print("-------------------")
+                applepics = []
+                for file in files:
+                    if "_crop.png" in file:
+                        picpath = subapplepath + "\\" + file
+                        applepics.append(picpath)
+
+# print(len(applepics))
+# print(applepics[0])
 #image_count = len(list(data_dir.glob('*/*.jpg')))
 #print(image_count)
 
@@ -57,13 +56,15 @@ print(applepics[0])
 # PIL.Image.open(str(apples[0]))
 #print(str(apples[0]))
 
-os.mkdir(applepath + "\\crop")
-os.mkdir(applepath + "\\crop\\apples")
+
+        os.mkdir(applepath + "\\crop\\apples")
 
 print(applepics[0])
-for pic in applepics:
-    if "_crop.png" in pic:
-        Path(pic).rename(pic.replace("apple_1", "crop\\apples\\", 1))
+
+
+        #for pic in applepics:
+        #    if "_crop.png" in pic:
+        #        Path(pic).rename(pic.replace("apple_1", "crop\\apples\\", 1))
 
 batch_size = 32
 img_height = 180
@@ -100,3 +101,5 @@ for images, labels in train_ds.take(1):
         plt.show()
         # plt.title(class_names[labels[i]])
         plt.axis("off")
+
+Path.exists()
