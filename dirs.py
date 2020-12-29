4511f6a6fc8e117662a4c608c8115486086bfd48
FelixOesinghaus <58511024+FelixOesinghaus@users.noreply.github.com>
2020-12-29 17:19:34 +0100

    Added depthcrop function

--- dirs.py
+++ dirs.py
@@ -72,6 +72,52 @@ def create_crop_directory(paths, dirs, files):
 			for pic in object_pics:
 				if "_crop.png" in pic:
 					Path(pic).rename(pic.replace(current_path, new_path, 1))
+def create_depthcrop_directory(paths, dirs, files):
+	main_crop_path = paths + "\\depthcrop"
+	os.mkdir(main_crop_path)
+	print("paths: ", paths)
+	print("dirs: ", dirs)
+	print("files: ", files)
+
+	for directory in dirs:
+		if "rgbd-dataset" in directory:
+			dataset_path = os.getcwd() + "\\" + directory
+			print(dataset_path)
+
+	data_dir = pathlib.Path(dataset_path)
+
+	paths, dirs, files = next(os.walk(dataset_path))
+	print("paths: ", paths)
+	print("dirs: ", dirs)
+	print("files: ", files)
+
+	for directory in dirs:
+		print(directory)
+		os.mkdir(main_crop_path + "\\" + directory)
+		sub_path = paths + "\\" + directory
+		print(sub_path)
+		sub_data_dir = pathlib.Path(sub_path)
+		
+		subpaths, subdirs, subfiles = next(os.walk(sub_path))
+		print("subpaths: ", subpaths)
+		print("subdirs: ", subdirs)
+		#print("subfiles: ", subfiles)
+		for subdirectory in subdirs:
+			sub_sub_path = sub_path + "\\" + subdirectory
+			print(sub_sub_path)
+			objectpaths, objectdirs, objectfiles = next(os.walk(sub_sub_path))
+			
+			object_pics = []
+			for file in objectfiles:
+				if "_depthcrop.png" in file:
+					picpath = sub_sub_path + "\\" + file
+					object_pics.append(picpath)
+			print(len(object_pics))
+			current_path = "rgbd-dataset\\" + directory + "\\" + subdirectory
+			new_path = "depthcrop\\" + directory
+			for pic in object_pics:
+				if "_depthcrop.png" in pic:
+					Path(pic).rename(pic.replace(current_path, new_path, 1))
 
 
 paths, dirs, files = next(os.walk(os.getcwd()))
@@ -80,6 +126,15 @@ if Path.exists(Path(os.getcwd() + "\\crop")):
 	print("./crop existiert bereits.")
 else:
 	create_crop_directory(paths, dirs, files)
+	
+# if depthcrop does not exist, use create_depthcrop_directory
+if Path.exists(Path(os.getcwd() + "\\depthcrop")):
+	print("./depthcrop existiert bereits.")
+else:
+	create_depthcrop_directory(paths, dirs, files)
+
+
+
 
 crop_path = paths + "\\crop"
 
